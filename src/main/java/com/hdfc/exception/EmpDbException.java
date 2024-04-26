ef31c4e7e4d51cf8dc6f4d4f8572fc17daaf8502
kchaitanya.48 <kchaitanya.48@outlook.com>
2024-04-27 00:51:21 +0530

    teting

--- src/main/java/com/hdfc/exception/EmpDbException.java
+++ src/main/java/com/hdfc/exception/EmpDbException.java
@@ -2,9 +2,15 @@ package com.hdfc.exception;
 
 import com.hdfc.service.EmployeeService;
 
+import javax.swing.*;
+
 public class EmpDbException extends Exception{
 
     public EmpDbException(String msg){
+
+
         super(msg);
+        System.out.println("msg"+msg);
+
     }
 }
